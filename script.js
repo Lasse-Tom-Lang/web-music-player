8d7578decee20fa55bd6bc351bcc3e4b0e032e3e
Lasse Lang <lang.lasse@gmx.de>
2022-07-02 17:57:06 +0200

    Added autoplay and audio speed

--- script.js
+++ script.js
@@ -15,15 +15,45 @@ currentClip = 0;
 
 isPlaying = false;
 
+autoplay = false;
+
+playbackSpeed = document.getElementById("btn-playbackSpeed");
+playbackSpeed.addEventListener("click", () => {
+  switch (audio.playbackRate) {
+    case 1:
+      audio.playbackRate = 1.5;
+      break;
+    case 1.5:
+      audio.playbackRate = 2;
+      break;
+    case 2:
+      audio.playbackRate = 0.5;
+      break
+    case 0.5:
+      audio.playbackRate = 1;
+      break;
+  }
+  playbackSpeed.innerHTML = audio.playbackRate + "x";
+});
+
+autoplayImg = document.querySelector("#btn-autoplay img");
+document.getElementById("btn-autoplay").addEventListener("click", () => {
+  autoplay = autoplay?false:true;
+  autoplayImg.src = autoplay?"icons/playWhite.png":"icons/playNot.png";
+});
+
 function setClip() {
+  playbackRate = audio.playbackRate;
   audio.src = "music/" + musicClips[currentClip].audio;
   cover.src = "music/" + musicClips[currentClip].cover;
   title.innerHTML = musicClips[currentClip].name;
+  audio.playbackRate = playbackRate;
 }
 
 function loadSong(song) {
   currentClip = song;
   setClip();
+  isPlaying?audio.play():audio.pause();
 }
 
 function setTrackList() {
@@ -51,18 +81,30 @@ title = document.getElementById("title");
 
 btnPlaypause_img = document.querySelector("#btn-playpause img");
 
-audio.addEventListener("timeupdate", (e) => {
-  currentPercent =  e.srcElement.currentTime / e.srcElement.duration * 100;
+audio.addEventListener("timeupdate", () => {
+  currentPercent =  audio.currentTime / audio.duration * 100;
   progressBar.style.transform = "translateX(" + (currentPercent - 100) + "%)";
+  if (audio.currentTime == audio.duration && autoplay) {
+    currentClip = ((currentClip + 1) == musicClips.length)?0:currentClip += 1;
+    setClip();
+    audio.play();
+  }
+  if (audio.currentTime == audio.duration && !autoplay) {
+    isPlaying = false;
+    btnPlaypause_img.src = "icons/play.png";
+    audio.pause();
+  }
 });
 
 document.getElementById("btn-playpause").addEventListener("click", () => {
   switch (isPlaying) {
-    case true: isPlaying = false;
+    case true:
+      isPlaying = false;
       btnPlaypause_img.src = "icons/play.png";
       audio.pause();
       break;
-    case false: isPlaying = true;
+    case false:
+      isPlaying = true;
       btnPlaypause_img.src = "icons/pause.png";
       audio.play();
       break;
