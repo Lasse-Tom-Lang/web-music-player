272a43e9a37100ea0f256e056ee36f5128997ea1
Lasse Lang <lang.lasse@gmx.de>
2022-11-16 20:16:36 +0100

    Added server

---
+++ index.ts
@@ -0,0 +1,24 @@
+import express from "express"
+const app = express()
+import fs from "fs"
+
+app.listen(80, () => {
+  console.log("Server started on port 80")
+})
+
+app.use('/icons', express.static('./public/icons'))
+
+app.get('/', (req, res) => {
+  res.write(fs.readFileSync("./public/index.html"));
+  res.end();
+});
+
+app.get('/style.css', (req, res) => {
+  res.write(fs.readFileSync("./public/style.css"));
+  res.end();
+});
+
+app.get('/script.js', (req, res) => {
+  res.write(fs.readFileSync("./public/script.js"));
+  res.end();
+});
