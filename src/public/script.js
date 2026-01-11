a419a12676655b83d3716952b782dd6b74d3b644
Lasse Lang <lang.lasse@gmx.de>
2026-01-11 21:00:19 +0100

    Added first playback function

--- src/public/script.js
+++ src/public/script.js
@@ -1,18 +1,39 @@
 const playButton = document.getElementById("playButton");
 const playBar = document.getElementById("playBar");
 const playBarFill = document.getElementById("playBarFill");
+const mainAudioSource = document.getElementById("mainAudioSource");
 
-function playAudio(event) {
-  playButton.src = "icons/pause.svg";
+let audioIsPlaying = true
+
+function toggleAudio() {
+  if (audioIsPlaying) {
+    playButton.src = "icons/pause.svg";
+    audioIsPlaying = false;
+    mainAudioSource.play()
+    return;
+  }
+  playButton.src = "icons/play.svg";
+  audioIsPlaying = true;
+    mainAudioSource.pause()
 }
 
 function setPlayBarFill(scaleFactor) {
   playBarFill.style.width = `${scaleFactor*100}%`;
 }
 
-playBar.addEventListener("click", (event) => {
+function audioPlaying() {
+  setPlayBarFill(mainAudioSource.currentTime / mainAudioSource.duration);
+  if (mainAudioSource.currentTime >= mainAudioSource.duration) {
+    toggleAudio();
+  }
+}
+
+playBar.addEventListener("mousedown", (event) => {
   let newAudioPosition = ((event.clientX-playBar.getBoundingClientRect().left) / playBar.scrollWidth);
   setPlayBarFill(newAudioPosition);
+  mainAudioSource.currentTime = mainAudioSource.duration * newAudioPosition;
 })
 
-playButton.onclick = playAudio;
+playButton.onclick = toggleAudio;
+
+mainAudioSource.ontimeupdate = audioPlaying;
