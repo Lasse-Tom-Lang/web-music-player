c06d44aa45f0a9ea6935f9049a95ad2ed8d16b9d
Lasse Lang <lang.lasse@gmx.de>
2026-01-17 22:18:04 +0100

    Added artists API route

--- src/database.js
+++ src/database.js
@@ -1,12 +1,32 @@
-import { DatabaseSync } from 'node:sqlite';
+import Database from 'better-sqlite3';
 
-const database = new DatabaseSync('./database.db');
+const database = new Database('./database.db');
 
 const initDatabase = `
-  CREATE TABLE IF NOT EXISTS atrists (
-    artist_name TEXT PRIMARY KEY UNIQUE,
+  CREATE TABLE IF NOT EXISTS artists (
+    artist_id TEXT PRIMARY KEY UNIQUE,
+    artist_name TEXT,
     artist_cover_image TEXT
+  );
+  CREATE TABLE IF NOT EXISTS albums (
+    album_id TEXT PRIMARY KEY UNIQUE,
+    album_name TEXT,
+    album_cover_image TEXT,
+    artist_id TEXT,
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
   );
 `;
 
 database.exec(initDatabase);
+
+const addArtist = database.prepare(`
+  INSERT INTO artists (artist_id, artist_name, artist_cover_image)
+  VALUES (?, ?, ?)
+`);
+
+const getArtists = database.prepare('SELECT * FROM artists');
+
+export {
+  addArtist,
+  getArtists
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 const app = express();
 import fs from "fs";
+import {addArtist, getArtists} from "./database.js"
 
 app.listen(80, () => {
   console.log("Server started on port 80");
 });
 
 app.use('/', express.static('./public/'));
+
+app.get("/api/getAllArtists", (req, res) => {
+  let allArtists = getArtists.all();
+  res.status(200).json(allArtists);
+});
