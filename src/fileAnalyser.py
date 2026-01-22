fd2e9ea5c4ff7a6bb02387ad55571cf47287fa0e
Lasse Lang <lang.lasse@gmx.de>
2026-01-22 21:19:58 +0100

    Added database actions for analyser

--- src/fileAnalyser.py
+++ src/fileAnalyser.py
@@ -3,6 +3,15 @@ import os
 from colorama import init as colorama_init
 from colorama import Fore
 from colorama import Style
+import sqlite3
+import re
+
+connection = sqlite3.connect("analyser.db")
+cursor = connection.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS artists (artist_id TEXT PRIMARY KEY UNIQUE,artist_name TEXT,artist_cover_image TEXT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS albums (album_id TEXT PRIMARY KEY UNIQUE,album_name TEXT,album_cover_image TEXT,artist_id TEXT,FOREIGN KEY (artist_id) REFERENCES artists(artist_id))")
+cursor.execute("CREATE TABLE IF NOT EXISTS audioTracks (audioTrack_id TEXT PRIMARY KEY UNIQUE,audioTrack_name TEXT,audioTrack_number Int,audioTrack_location TEXT,album_id TEXT,FOREIGN KEY (album_id) REFERENCES albums(album_id))")
 
 basedir = ""
 
@@ -13,6 +22,8 @@ for artist in artists:
   if artist == ".DS_Store":
     continue
   print(Fore.GREEN + artist + Style.RESET_ALL)
+  command = f"INSERT INTO artists (artist_id, artist_name, artist_cover_image) VALUES ('{re.sub('\\W+','', artist.lower())}', '{artist}', NULL)"
+  cursor.execute(command)
   if not os.path.isdir(basedir + artist):
     continue
   albums = os.listdir(basedir + artist)
@@ -21,6 +32,8 @@ for artist in artists:
     if album == ".DS_Store":
       continue
     print(Fore.BLUE + album + Style.RESET_ALL)
+    command = f"INSERT INTO albums (album_id, album_name, album_cover_image, artist_id) VALUES ('{re.sub('\\W+','', artist.lower()) + re.sub('\\W+','', album.lower())}', '{album}', NULL, '{re.sub('\\W+','', artist.lower())}')"
+    cursor.execute(command)
     if not os.path.isdir(basedir + artist + "/" + album):
       continue
     tracks = os.listdir(basedir + artist + "/" + album)
@@ -30,6 +43,9 @@ for artist in artists:
         continue
       audio = EasyID3(basedir + artist + "/" + album + "/" + track)
       if audio != {}:
+        # print(audio)
         print(f"File '{track}': '{audio["title"][0]}' by '{audio["artist"][0]}', Album '{audio["album"][0]}'")
       else:
         print(track)
+
+connection.commit()
